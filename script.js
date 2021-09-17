e51916469e30cbf34ad5de45a3df68ccad01898d
Mohit Jaisal <mohitjaisal19@gmail.com>
2021-09-18 02:20:00 +0530

    JS event listener

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const listItems = []
+
+getData()
+
+filter.addEventListener('input', (e) => filterData(e.target.value))
